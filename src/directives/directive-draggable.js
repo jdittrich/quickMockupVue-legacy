09d1fae8f2873ae471565240d04d57baf87d436e
jand <d_jan@ymail.com>
2016-05-29 14:44:24 +0200

    added mixin, directives

---
+++ src/directives/directive-draggable.js
@@ -0,0 +1,32 @@
+//only drags
+import interact from 'interact.js'
+import vue from 'vue'
+
+Vue.directive('draggable', {
+  //we need a two way bind here to change the values from within the directives functions!
+  twoWay:true, //? <-> or better just dispatch?
+  bind: function () {
+    // do preparation work
+    // e.g. add event listeners or expensive stuff
+    // that needs to be run only once
+    var that = this;
+    var el = that.el;
+    interact(that.el)
+      .draggable({
+        autoscroll:true,
+        onmove:dragmoveListener,
+        onend:dragendListener,
+      .on('eventname', function (event) {
+      });
+  },
+  update: function (newValue, oldValue) {
+
+    // do something based on the updated value
+    // this will also be called for the initial value
+  },
+  unbind: function () {
+    // do clean up work
+    // e.g. remove event listeners added in bind()
+    interact(this.el).unset();
+  }
+})
---
+++ src/directives/directive-dropable.js
@@ -0,0 +1,5 @@
+//imports actions: add new child, move child 
+
+
+//finds out: new child? (or do that in the directive?)
+//drop : add to new parent OR move OR just add to parent all the time, no matter if new or old, we could handle this on the data level. 
