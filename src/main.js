64cc131b878a590bd5657cdb1ad1d2424098ed49
jan d <d_jan@ymail.com>
2016-05-30 22:24:40 +0200

    debug statement

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './vuex/store.js'
 
+
 new Vue({
   store:store,
   el: 'body',
   components: { App }
 })
+
+Vue.config.debug = true
