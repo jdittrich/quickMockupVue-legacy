ccd472cfafadcb4a62e52252faab74eb7634a407
jand <d_jan@ymail.com>
2016-07-12 21:07:44 +0200

    added inlineedit component

---
+++ src/components/widgetinlineedit.vue
@@ -0,0 +1,36 @@
+<template>
+    <span>
+        <span v-on:dblclick="enterEdit" v-show="!editMode" >Hello!!!{{text}}</span>
+        <span v-show="editMode">
+            <input type="text" value={{text}}>
+            <span v-on:click="exitEdit">OK</span>
+        </span>
+    </span>
+    <!-- spans all over the place? -->
+</template>
+<script>
+
+export default {
+    name:"widgetInlineEdit",
+    props: {
+        "text":String //could also have a classname with a default
+    },
+    data(){
+        return{
+            editMode:false
+        }
+    },
+    template: '',
+    methods:{
+        enterEdit(){
+            this.editMode = true;
+        },
+        exitEdit(){
+            this.editMode = false;
+            this.text = this.$el.querySelectorAll("input")[0].value;
+        }
+    }
+}
+</script>
+<style>
+</style>
