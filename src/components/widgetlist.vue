9ff40fab07334fd1bc3b550fe1df0c8d0b8f1c53
jan d <d_jan@ymail.com>
2016-07-29 21:55:11 +0200

    add widgetlist

---
+++ src/components/widgetlist.vue
@@ -0,0 +1,50 @@
+<template>
+    <ul>
+        <slot>slot!</slot>
+    </ul>
+    <button v-on:click="addButtonHandler">+</button>
+</template>
+<script>
+
+
+import {addWidgetArrayItem as addArrayItem} from '../vuex/actions.js'
+
+//should import inline edit
+
+export default {
+    name:"widgetlist",
+    created(){
+
+    },
+    compiled(){
+
+    },
+    props: {
+        "list": Array,
+    },
+    data(){
+        return{
+            editMode:false
+        }
+    },
+    methods:{
+        addButtonHandler:function(event){
+            this.addArrayItem(this.list);
+        },
+        enterEdit(){
+            this.editMode = true;
+        },
+        exitEdit(){
+            this.editMode = false;
+        }
+    },
+    vuex:{
+        getters:{},
+        actions:{
+            addArrayItem:addArrayItem
+        }
+    }
+}
+</script>
+<style>
+</style>
