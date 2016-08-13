5a307476e04856d8687268a36a35ce4c00933313
jan d <d_jan@ymail.com>
2016-08-13 20:45:05 +0200

    added toggle component

---
+++ src/components/widgetToggleState.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        STATE:{{state}}<input type="checkbox" v-on:click="switchState" {{state ? checked : }}> <!-- if state is true, write "checked", otherwise write nothing-->
+    <div>
+</template>
+<script>
+
+import toggleWidgetState from '../vuex/actions.js';
+
+//should import inline edit
+
+export default {
+    name:"widgettogglestate",
+    props:{
+        "state":Object,
+        "list":Array,
+    },
+    methods:{
+        switchState:function(event){
+            this.toggleWidgetState(this.state, this.list);
+        }
+    },
+    vuex:{
+        getters:{},
+        actions:{
+            toggleWidgetState:function(store,state,array){
+                store.dispatch("TOGGLEWIDGETSTATE",state,array);
+            },
+        }
+    }
+};
+</script>
+<style>
+</style>
